943e7891770e4f9a6f75e11f2819c40ce8b19584
Marcoswrc2 <marcoswrc2@gmail.com>
2021-12-09 11:00:05 -0300

    Config: add vector icons to application

--- src/screens/ConfigScreen/index.js
+++ src/screens/ConfigScreen/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {useTheme} from 'styled-components';
 import {TextSemiBold} from '../../styles/components';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const ConfigScreen = () => {
   const {colors} = useTheme();
